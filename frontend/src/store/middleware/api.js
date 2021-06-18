a9b6c64c34268241f9d179f089160e5bc03e6ec5
Nate Nelson <7nater.nelson@gmail.com>
2021-06-17 19:04:20 -0600

    API Middleware Formating

--- frontend/src/store/middleware/api.js
+++ frontend/src/store/middleware/api.js
@@ -3,33 +3,44 @@ import axios from 'axios'
 import * as actions from '../api'
 
 const constructHeader = getStateFn => headers => {
+    //Construct an Request header with standard content, available token, and/or optional headers
     const token = getStateFn().auth.token
-    console.log({state: getStateFn()})
     const auth = {'Authorization': `Bearer ${token}`}
     const standardHeader = {"Content-Type":"application/json"}
-    const newHeaders =  headers && token ? {...headers, ...auth, ...standardHeader} : 
+    return (
+        headers && token ? {...headers, ...auth, ...standardHeader} : 
         headers && !token ? {...headers, ...standardHeader} :
         !headers && token ? {...auth, ...standardHeader} : 
-        standardHeader 
-
-    console.log(newHeaders)
-
-    return newHeaders
+        standardHeader
+        ) 
 }
 
 const api = ({dispatch, getState}) => next => async action => {
+    //Check for API Request
     if (action.type !== actions.apiCallBegan.type) return next(action)
     next(action)
-    let {url, method, data, onSuccess, headers, onStart, onError} = action.payload
+
+    //Before Request Dispatches
     if(onStart) Array.isArray(onStart) ? onStart.forEach(type => dispatch({type, payload: data})) : dispatch({onStart, payload: data})
     
-    headers = constructHeader(getState)(headers)
+    //Begin API Request
+    let {url, method, data, onSuccess, headers, onStart, onError} = action.payload
+    const options = {
+        baseURL: process.env.REACT_APP_BASE_URL, 
+        url, 
+        method, 
+        data, 
+        headers: constructHeader(getState)(headers) 
+    }
     try {
-        const res = await axios.request({baseURL: process.env.REACT_APP_BASE_URL, url, method, data, headers})
-        // console.log(res)
+        //Request
+        const res = await axios.request(options)
+        //After Request Dispatches 
         dispatch(actions.apiCallSuccess(res.data))
         if(onSuccess) dispatch({type: onSuccess, payload: res.data})
+
     } catch(error) {
+        //Error Request Dispatches
         dispatch(actions.apiCallFailed(error))
         if(onError) dispatch({type: onError, payload: error})
     }
